f7ff654c4587f545c7b850f92c41a02e972f2e08
Bruno Sainz <martins-sainzb@mssm.org>
2022-02-04 09:43:10 -0500

    initial commit

---
+++ cubesDisape/Assets/move.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float moveSpeed = 10;
+
+    public GameObject bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed);
+  
+if (Input.GetButtonDown("Fire1")) {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position + Vector3.up*2, transform.rotation);
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.forward * 30;
+            Destroy(bullet, 4);
+        }
+   }
+}
